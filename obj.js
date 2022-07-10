eba451791ef0982e2704e647553a10cc150cc2d7
Jean De Dieu Iradukunda <84918755+IradJean@users.noreply.github.com>
2022-07-10 19:10:11 +0200

    Add files via upload
    committing Js code about Objects// use of object.keys() and object.values()

---
+++ obj.js
@@ -0,0 +1,22 @@
+// Objects in depth
+/*
+declaring objects with new keyword
+*/
+const myNewObj = new object();
+
+const myVarAge = myNewObj.age = 22;
+const myVarDept = myNewObj.dept = 'Computer Science';
+const myVarFavLang = myVarmyNewObj.favProgrammingLanguage = 'Javascript';
+const myVar = myNewObj.name = 'Jean';
+console.log(myVarName);
+console.log(myVarDept);
+console.log(myVarAge);
+console.log(myVarFavLang);
+// object methods/functions===> object.keys() vs object.values()
+const dictionary = {
+  car: 'automobile',
+  apple: 'healthy snack',
+  cat: 'cute furry animal',
+  dog: 'best friend'
+};
+object.keys(dictionary);
