f419a23d63e0190cd3a2ee39a39da7c1df735d3d
IradJean <jadongenda@gmail.com>
2022-07-11 12:34:25 +0200

    updating callbacks

--- functionCallBacks.js
+++ functionCallBacks.js
@@ -27,6 +27,12 @@ each([-2,-5,3,6,-7,10, -9], isPostive)
 //array.forEach(function callback(currentValue, index, array) {
     // function code here
 // });
+//changing the levels of an array letters (toUpperCase()) 
+const myArray = ['hello', 'Peter', 'Paul', 'Jean', 'Divine'];
+const NamesChanged = myArray.forEach(function(name){
+	return name.toUpperCase();
+});
+console.log(NamesChanged);
 
 //ex: a functon that takes in a number and logs it to the console if it is even:
 function logEvenNum(num){
@@ -62,3 +68,39 @@ const namesLengths = Names.map(function(name){
 
 console.log(namesLengths);
 // returning the UPPER CASE OF AN ARRAY;
+const NamesUpper = Names.map(function(name){
+	return name.toUpperCase();
+})
+// Annoying challenge
+/* Using map()
+ *
+ * Using the musicData array and map():
+ *   - Return a string for each item in the array in the following format:
+ *     <album-name> by <artist> sold <sales> copies
+ *   - Store the returned data in a new albumSalesStrings variable
+ *
+ * Note:
+ *   - Do not delete the musicData variable
+ *   - Do not alter any of the musicData content
+ *   - Do not format the sales number; leave it as a long string of digits
+ */
+
+const musicData = [
+    { artist: 'Adele', name: '25', sales: 1731000 },
+    { artist: 'Drake', name: 'Views', sales: 1608000 },
+    { artist: 'Beyonce', name: 'Lemonade', sales: 1554000 },
+    { artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000 },
+    { artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000 },
+    { artist: 'Original Broadway Cast Recording', 
+      name: 'Hamilton: An American Musical', sales: 820000 },
+    { artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000 },
+    { artist: 'Prince', name: 'The Very Best of Prince', sales: 668000 },
+    { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+    { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+];
+
+const albumSalesStrings = musicData.map(function(alb){
+    return alb.artist +" "+ alb.sales;
+});
+
+console.log(albumSalesStrings);
